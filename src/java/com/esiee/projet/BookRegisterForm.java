6e4e00df6b09e1197268133c282560e4712d661a
picaud david <david.picaud@edu.esiee.fr>
2014-03-22 16:23:09 +0100

    panier

--- src/java/com/esiee/projet/BookRegisterForm.java
+++ src/java/com/esiee/projet/BookRegisterForm.java
@@ -5,11 +5,29 @@
  */
 
 package com.esiee.projet;
-
+import javax.servlet.http.HttpServletRequest;
 /**
  *
  * @author zhao
  */
-public class BookRegisterForm {
+public final class BookRegisterForm extends Form{
     
+        private static final String CHAMP_TITRE = "titre";
+	private static final String CHAMP_AUTEUR = "auteur";
+        private static final String CHAMP_GENRE = "genre";
+	private static final String CHAMP_DESCRIPTION = "description";
+        private static final String CHAMP_PRIX = "prix";
+	private static final String CHAMP_TYPE = "type";
+	
+	private DAO dao;
+	private Utilisateur utilisateur;
+	
+	public BookRegisterForm( DAO utilisateurDao ) {
+		super();
+		this.utilisateur = new Utilisateur();
+		this.dao = utilisateurDao;
+	}
+    
+        
+        public Livre enregistrerBook( HttpServletRequest request ){}
 }
--- web/WEB-INF/achat.jsp
+++ web/WEB-INF/achat.jsp
@@ -9,7 +9,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>jQuery UI Spinner - Default functionality</title>
+<title>Panier</title>
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 
 <!-- Optional theme -->
