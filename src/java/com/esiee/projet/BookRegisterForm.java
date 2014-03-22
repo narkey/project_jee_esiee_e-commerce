d7222689250daa2c7428251ed2925bd38bd6bb16
picaud david <david.picaud@edu.esiee.fr>
2014-03-22 17:20:51 +0100

    bookregisterform fait

--- src/java/com/esiee/projet/BookRegisterForm.java
+++ src/java/com/esiee/projet/BookRegisterForm.java
@@ -20,14 +20,125 @@ public final class BookRegisterForm extends Form{
 	private static final String CHAMP_TYPE = "type";
 	
 	private DAO dao;
-	private Utilisateur utilisateur;
+	private Livre livre;
 	
-	public BookRegisterForm( DAO utilisateurDao ) {
+	public BookRegisterForm( DAO livreDao ) {
 		super();
-		this.utilisateur = new Utilisateur();
-		this.dao = utilisateurDao;
+		this.livre = new Livre();
+		this.dao = livreDao;
 	}
     
         
-        public Livre enregistrerBook( HttpServletRequest request ){}
+        public Livre enregistrerBook( HttpServletRequest request ){
+            
+                String titre = getValeurChamp( request, CHAMP_TITRE );
+		String auteur = getValeurChamp( request, CHAMP_AUTEUR );
+                String genre = getValeurChamp( request, CHAMP_GENRE );
+		String description = getValeurChamp( request, CHAMP_DESCRIPTION );
+                String prix = getValeurChamp( request, CHAMP_PRIX );
+		String type = getValeurChamp( request, CHAMP_TYPE );
+	
+                
+		traiterTitre( titre, livre );
+		traiterAuteur( auteur, livre );
+		traiterGenre( genre, livre );
+		traiterDescription( description, livre );
+                traiterPrix( prix, livre );
+                
+                
+			
+		if ( erreurs.isEmpty() ) {
+                    try {
+                            validationChampRequis(type);
+                    } 
+                    catch ( Exception e ) {
+                            setErreur( CHAMP_TYPE , e.getMessage() );
+                    }
+
+                    if(type.equals("Mangas")){
+                        Manga manga = new Manga(livre.getId(),titre,auteur,genre,description,Float.parseFloat(prix));
+                        return (Livre)manga;
+                    }
+                    
+                    if(type.equals("Comics")){
+                       Bd bd = new Bd(livre.getId(),titre,auteur,genre,description,Float.parseFloat(prix));
+                       return (Livre)bd;
+                    }
+                    
+                    if(type.equals("BD")){
+                        Comic comic = new Comic(livre.getId(),titre,auteur,genre,description,Float.parseFloat(prix));
+                        return (Livre)comic;
+                    }
+
+		}
+                
+                return null;
+	}
+
+        
+        
+        
+	
+	private void traiterTitre( String titre, Livre livre) {
+		
+		try {
+			validationChampRequis(titre);
+		} 
+		catch ( Exception e ) {
+			setErreur( CHAMP_TITRE, e.getMessage() );
+		}
+		
+		livre.setTitre( titre );
+	}
+	
+        private void traiterAuteur( String auteur, Livre livre) {
+		
+		try {
+			validationChampRequis(auteur);
+		} 
+		catch ( Exception e ) {
+			setErreur( CHAMP_AUTEUR, e.getMessage() );
+		}
+		
+		livre.setAuteur( auteur );
+	}
+        
+        private void traiterGenre( String genre, Livre livre) {
+		
+		try {
+			validationChampRequis(genre);
+		} 
+		catch ( Exception e ) {
+			setErreur( CHAMP_GENRE, e.getMessage() );
+		}
+		
+		livre.setTitre( genre );
+	}
+        
+         private void traiterDescription( String description, Livre livre) {
+		
+		try {
+			validationChampRequis(description);
+		} 
+		catch ( Exception e ) {
+			setErreur( CHAMP_DESCRIPTION, e.getMessage() );
+		}
+		
+		livre.setTitre( description );
+	}
+      
+        private void traiterPrix( String prix, Livre livre) {
+		
+		try {
+			validationChampRequis(prix);
+		} 
+		catch ( Exception e ) {
+			setErreur( CHAMP_PRIX , e.getMessage() );
+		}
+		
+		livre.setTitre( prix );
+	}
+       
+	
 }
+
